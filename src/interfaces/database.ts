b969cc4d004e8fcbd6e9cfb4daf1698d4c5b2b95
Daniel Enrique Gomez Alcala <alu0101547961@ull.edu.es>
2025-03-23 17:54:02 +0000

    Pruebas

--- src/interfaces/database.ts
+++ src/interfaces/database.ts
@@ -4,6 +4,5 @@ import { Coleccion } from "./coleccion.js";
  * Interfaz para gestionar una base de datos
  */
 export interface Database<T> extends Coleccion<T> {
-  initialize(data: T[]): void;
   actualizarBase(): void;
 }
